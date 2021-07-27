727d0de880bb96c90d331ed3df0d94e255397afe
Asdf <dino.muslic51@gmail.com>
2021-07-27 15:08:06 +0200

    second commit

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-let buttons = document.querySelectorAll('.btn');
+const buttons = document.querySelectorAll('.btn');
 let display = document.querySelector('.display');
 const clear = document.getElementById('clear');
 
-let equation = ``;
+let result = ``;
 buttons.forEach(button => {
     button.addEventListener('click', () => {
 
-        if(!isNaN(button.innerHTML)){
+        if(!isNaN(button.innerHTML)) {
             display.value += `${button.innerHTML}`;
-            equation += button.innerHTML;
-        }else{
+            result += button.innerHTML;
+        }else {
             display.value = ``;
             if(button.innerHTML !== '=' && button.innerHTML !== 'C'){
-                equation += button.innerHTML;
-            }else if(button.innerHTML === 'C'){
-                equation = ``;
-            }else{
-                display.value = eval(equation);
-                equation = parseFloat(display.value);
+                result += button.innerHTML;
+            } else if(button.innerHTML === 'C'){
+                result = ``;
+            } else{
+                display.value = eval(result);
+                result = parseFloat(display.value);
             }
         }
+        
+        // console.log('result: ', result);
     })
 });
 
 clear.addEventListener('click', () => {
     display.value = ``;
-    equation = [];
-})
+    result = [];
+});
